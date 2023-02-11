53c6a03be2030724f5f0533adf5547a364ccde1a
cgcouto <68869586+cgcouto@users.noreply.github.com>
2023-02-11 06:26:03 +0000

    Finished part one!

--- src/main/scala/collatz/Collatz.scala
+++ src/main/scala/collatz/Collatz.scala
@@ -1,1 +1,8 @@
+def collatz(n : Int) = n % 2 match
+    case 0 => n / 2
+    case 1 => 3*n + 1
 
+
+def collatzCount(n : Int): Int = n match
+    case 1 => 0
+    case _ => collatzCount(collatz(n))+1
